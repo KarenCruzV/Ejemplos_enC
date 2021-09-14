54ce6e11d38c5e85cb7d33e5706fcab920f87452
KarenCruzV <al2173075040@azc.uam.mx>
2021-09-14 14:46:00 -0500

    Hola mundo en C

---
+++ main.c
@@ -0,0 +1,20 @@
+/*Comentarios son para nosotros del futuro
+Nombre del programa
+Autor
+Modificado por
+Que hace el programa
+Necesita un circuito?
+- Colocar una imagen en el repositorio_liga
+Terminales que se van a usar, el modo
+- Ejemplo: gpio 5 salida*/
+
+/*Bibliotecas*/
+#include <stdio.h>
+
+/*Declaracion de variables con el tipo correspondiente*/
+
+/*Estructura main*/
+int main(){
+    printf("Hola UAM");
+    return 0;
+}
